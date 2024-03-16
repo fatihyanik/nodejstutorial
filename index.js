fa4b69ed3c9d76f0f8f65f542bba44b1531d624a
fatihyanik <fatihyanik07@gmail.com>
2024-03-16 11:34:49 +0100

    added simple setup

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-file
+const http = require("http");
+
+const server = http.createServer((req, res)=>{
+    if(req.url === "/"){
+        res.write("Hello World");
+        res.end();
+    }
+
+    if(req.url === "/user"){
+        res.write("This is user page");
+        res.end();
+    }
+})
+
+server.listen(3000);
