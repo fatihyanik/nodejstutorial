f343a3b39e66fd5a7dfb7a55eda94558f184e613
fatihyanik <fatihyanik07@gmail.com>
2024-03-16 12:44:39 +0100

    added get and post request

--- index.js
+++ index.js
@@ -1,15 +1,53 @@
-const http = require("http");
+const express = require("express");
 
-const server = http.createServer((req, res)=>{
-    if(req.url === "/"){
-        res.write("Hello World");
-        res.end();
-    }
+const app = express();
+app.use(express.json());
 
-    if(req.url === "/user"){
-        res.write("This is user page");
-        res.end();
+const users = [
+    { id: 1, ad: "ahmet", yas: 35 },
+    { id: 2, ad: "mehmet", yas: 33 },
+    { id: 3, ad: "ali", yas: 31 },
+    { id: 4, ad: "veli", yas: 39 },
+    { id: 5, ad: "zeynep", yas: 7 },
+]
+
+app.get("/", (req, res) => {
+    res.send("<h1>Anasayfa</h1>");
+})
+
+app.get("/users", (req, res) => {
+
+    if (req.query.terscevir) {
+        res.send(users.reverse())
+    } else {
+        res.json(users);
     }
 })
 
-server.listen(3000);
+app.get("/users/:id", (req, res) => {
+    const id = req.params.id; // parametre alma
+
+    const foundedUser = users.find((user) => user.id === parseInt(id));
+    if (foundedUser) {
+        res.json(foundedUser)
+    } else {
+        res.status(404).send(id + " id'li kisi bulunamadi")
+    }
+})
+
+
+app.post("/users", (req, res) => {
+    const newUser = {
+        id: users.length + 1,
+        ad: req.body.ad,
+        yas: req.body.yas
+    }
+    users.push(newUser);
+    res.send(newUser)
+})
+
+app.get("*", (req, res) => {
+    res.send("<h1>404 Not Found</h1>");
+})
+
+app.listen(4000)
